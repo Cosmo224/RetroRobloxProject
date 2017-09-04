7151923480837201308192060460b1502cf3b392
DarkKnight64 <mario64crashed@gmail.com>
2017-09-04 10:17:34 +0100

    aaaa

---
+++ header.php
@@ -0,0 +1,14 @@
+<html>
+  <head>
+  <meta keywords="old roblox revival, old roblox, game like roblox, free online game">
+  <meta charset="utf-8">
+  <link rel="stylesheet" type="text/css" href="assets/css/global.css">
+</head>
+<body>
+  <img alt="Roblox Logo 2013" style="float: center;" src="assets/images/roblox.png">
+  <br>
+<ul>
+  <li id="navlink01"><a href="levels/">Build!></a></li>
+  <li id="navlink02"><a href="forum/">Forum</a></li>
+  <li id="navlink03"><a href="avatar/">Avatar</a></li>
+</ul>
---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+  <head>
+  <meta keywords="old roblox revival, old roblox, game like roblox, free online game">
+  <meta charset="utf-8">
+  <link rel="stylesheet" type="text/css" href="assets/css/global.css">
+</head>
+<body>
+  <?php include("header.php");?>
+  <p class="text-left" style="margin-left: 16px;">
+  <h2><strong>Welcome to the Retro Roblox Project!</strong></h2>
+  <br><br>
+  <h5><strong>- Play our games!</h5></strong>
+  <br>
+  <h5><strong>- Use the forums</h5></strong>
+  <br>
+  <h5><strong>- Message people!</h5></strong>
+  <br>
+  <h5><strong>- 100% guaranteed no drama! </h5></strong>
+</p>
+  <?php include("footer.php");?>
+</body>
+</html>
